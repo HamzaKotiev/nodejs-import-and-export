7433cf0e9159f0369762ffd2ff48797c01b37086
KhamzatKot <1000000000020z@gmail.com>
2022-11-07 20:48:45 +0300

    Первый таск

---
+++ first.js
@@ -0,0 +1,4 @@
+export const names = 'Бал';
+export const sername = 'ван';
+
+export let sum = () => names + sername;
---
+++ second.js
@@ -0,0 +1,4 @@
+import {sum} from './first.js'
+import {names} from './first.js'
+import {sername} from './first.js'
+console.log(sum(),names,sername)
